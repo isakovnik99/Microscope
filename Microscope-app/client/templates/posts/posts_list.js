60cea17dc8911a31aa20719f584b5ca3277165e3
DESKTOP-VKQ6BVT\isako <isakov.cpp@gmail.com>
2017-08-17 11:27:52 +0200

    Wired collection into `postsList` template.

--- Microscope-app/client/templates/posts/posts_list.js
+++ Microscope-app/client/templates/posts/posts_list.js
@@ -1,19 +1,21 @@
-var postsData =[
-     {
-    title: 'Introducing Telescope',
-    url: 'sachagreif.com/introducing-telescope/'
-}, 
-{
-    title: 'Meteor',
-    url: 'http://meteor.com'
+// var postsData =[
+//      {
+//     title: 'Introducing Telescope',
+//     url: 'sachagreif.com/introducing-telescope/'
+// }, 
+// {
+//     title: 'Meteor',
+//     url: 'http://meteor.com'
 
-},
-{
-    title: 'The meteor book',
-    url:'http://themeteorbook.com'
-}
-];
+// },
+// {
+//     title: 'The meteor book',
+//     url:'http://themeteorbook.com'
+// }
+// ];
 
 Template.postsList.helpers({
-    posts:postsData
+    posts: function(){
+        return Posts.find();
+    }
 });
